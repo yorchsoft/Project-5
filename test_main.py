32a9d679d807f0b54151b98e9a32972bdeaa4f49
Jorge Rodriguez <yorchsoft@gmail.com>
2020-06-08 21:52:18 +0900

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -31,7 +31,7 @@ def test_health(client):
 def test_auth(client):
     body = {'email': EMAIL,
             'password': PASSWORD}
-    response = client.post('/auth', 
+    response = client.post('/auth',
                            data=json.dumps(body),
                            content_type='application/json')
 
